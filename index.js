38e80a1840634cfee55bddb30e374bdd84c2f445
khadijun@DESKTOP <kardikhadija1@gmail.com>
2025-03-11 00:05:42 +0300

    phase-1-first-class-functions-lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function (drivers) {
+  return drivers.slice(0, 2);
+};
+console.log(returnFirstTwoDrivers(["Antonia", "Nuru", "Amari", "Mo"]));
+
+const returnLastTwoDrivers = function (drivers) {
+  return drivers.slice(-2);
+};
+console.log(returnLastTwoDrivers(["Antonia", "Nuru", "Amari", "Mo"]));
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = (mutiplier) => (fare) => fare * mutiplier;
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = (drivers, DifferentDriver) =>
+  DifferentDriver(drivers);
